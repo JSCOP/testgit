9057b302bfdbcc6d7836077acd53a48ccd9b7fe2
jisung <mineloop08@gmail.com>
2022-01-20 01:15:02 +0900

    다시시작

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/JSCOP/testgit/tree/test/Module/HiModule
-
-go 1.17
--- test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/JSCOP/testgit/tree/test/Module/HiModule/"
-)
-
-func main() {
-	HiModule.Hello("jisung")
-}
